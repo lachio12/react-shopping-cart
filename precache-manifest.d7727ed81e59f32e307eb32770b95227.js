8cb4ce577d3b53cf100e7cef8bbcd4ee362f8155
lachio12 <truongvanthong94@gmail.com>
2019-09-10 00:13:51 +0700

    first commit

---
+++ precache-manifest.d7727ed81e59f32e307eb32770b95227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd11294f339d61133cefa5a31726791e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c95585de20d4685fbdfd",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1c50f8482549c278f751",
+    "url": "/static/js/2.bcbf78f6.chunk.js"
+  },
+  {
+    "revision": "c95585de20d4685fbdfd",
+    "url": "/static/js/main.284a3822.chunk.js"
+  },
+  {
+    "revision": "0aba0a20435f6c351d61",
+    "url": "/static/js/runtime~main.cba712b5.js"
+  }
+]);
